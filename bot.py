c899331a04c1938e450c135125be576bd1a89a86
rdadesukro <rdade30@gmail.com>
2026-02-10 10:21:37 +0700

    bot discord siap deploy

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot sudah online")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong! Bot hidup 24 jam")
+
+bot.run(TOKEN)
